422d37ee7415ce0bac7ce948e435a105708bc0c1
grebano <simonemaschio01@gmail.com>
2023-05-20 10:08:58 +0200

    update code - add location permission management and automatic wifi enabling

--- Android_app/SmartHome/app/src/main/java/com/maiot/smarthome/MainActivity.java
+++ Android_app/SmartHome/app/src/main/java/com/maiot/smarthome/MainActivity.java
@@ -1,17 +1,34 @@
 package com.maiot.smarthome;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button bttLights = null;
     private Button bttShutters = null;
     private Button bttAutomations = null;
+
+    private WifiManager wifiManager = null;
+
+    private LocationManager locationManager = null;
+    private int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+
     private final int ACTIVITY_REQUEST_CODE = 1;
 
     @Override
@@ -28,6 +45,18 @@ public class MainActivity extends AppCompatActivity {
         bttAutomations = findViewById(R.id.bttAutomations);
         button_start_automations_activity();
 
+        // gestione dei permessi riguardanti la posizione
+        getLocationPermission();
+
+        // se il wifi è disattivato lo attivo e lo notifico all'utente
+        if(!wifiManager.isWifiEnabled()){
+            Toast.makeText(getApplicationContext(), "WiFi is Not enabled.  Enabling...", Toast.LENGTH_LONG).show();
+            wifiManager.setWifiEnabled(true);
+        }
+        // se la posizione è disattivata mostro il dialog per attivarla
+        if(!locationManager.isLocationEnabled()){
+            buildAlertMessageNoGps();
+        }
     }
 
     private void button_start_lights_activity() {
@@ -58,5 +87,73 @@ public class MainActivity extends AppCompatActivity {
             //per capire quale activity è stata lanciata (è un id in pratica)
             startActivity(intent);
         });
+    }
+
+    //-------------------------------PERMESSI-POSIZIONE------------------------------------------------------------
+
+    private void getLocationPermission()
+    {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        checkPermissions(""+ Manifest.permission.ACCESS_FINE_LOCATION, LOCATION_PERMISSION_REQUEST_CODE+0);
+    }
+
+    private void checkPermissions(String permission, int requestCode) {
+        if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission))
+                showExplanationAndRequestPermission("Permission Needed", "Rationale", permission, requestCode);
+            else
+                requestPermission(permission, requestCode);
+        }
+    }
+
+    private void showExplanationAndRequestPermission(String title,
+                                                     String message,
+                                                     final String permission,
+                                                     final int permissionRequestCode) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        requestPermission(permission, permissionRequestCode);
+                    }
+                });
+        builder.create().show();
+    }
+
+    private void requestPermission(String permissionName, int permissionRequestCode) {
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{permissionName}, permissionRequestCode);
+    }
+
+    private boolean hasAllPermissionsGranted(@NonNull int[] grantResults) {
+        for (int grantResult : grantResults) {
+            if (grantResult == PackageManager.PERMISSION_DENIED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
     }
 }
