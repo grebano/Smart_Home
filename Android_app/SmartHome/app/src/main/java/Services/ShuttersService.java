21a1f90c0d3f4116fad31f07b602043a3cb19d3e
grebano <simonemaschio01@gmail.com>
2023-05-20 10:30:39 +0200

    minor fix in shutters service launching

--- Android_app/SmartHome/app/src/main/java/Services/ShuttersService.java
+++ Android_app/SmartHome/app/src/main/java/Services/ShuttersService.java
@@ -1,11 +1,16 @@
 package Services;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+
+import com.maiot.smarthome.R;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -19,6 +24,7 @@ import Miscellaneous.Constants;
 public class ShuttersService extends Service {
 
     private final String TAG = "MyShuttersService";
+    private final String NOTIFICATION_CHANNEL_ID = "MyShuttersService_ID";
     public static boolean isRunning = false;
 
     private String currentTime;
@@ -44,6 +50,8 @@ public class ShuttersService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i(TAG,"Service - onStartCommand");
         isRunning = true;
+
+        addNotification();
 
         startCheckingTime();
 
@@ -80,5 +88,21 @@ public class ShuttersService extends Service {
         };
         // si schedula un'azione ogni 2sec senza delay iniziale
         timer.scheduleAtFixedRate(timerTask,Constants.TIMER_DELAY_IN_MILLIS,Constants.TIMER_PERIOD_IN_MILLIS);
+    }
+
+    private void addNotification(){
+        NotificationChannel channel = null;
+        channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_ID, NotificationManager.IMPORTANCE_LOW);
+        getSystemService(NotificationManager.class).createNotificationChannel(channel);
+
+        Notification.Builder notification = null;
+        notification = new Notification.Builder(
+                this, NOTIFICATION_CHANNEL_ID)
+                .setContentText("MyShuttersService is running")
+                .setContentTitle("Service is running")
+                .setOngoing(true)
+                .setSmallIcon(R.drawable.ic_launcher_background);
+
+        startForeground(1001,notification.build());
     }
 }
