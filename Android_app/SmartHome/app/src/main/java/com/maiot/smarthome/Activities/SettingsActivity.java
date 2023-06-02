0192b470789db2896264f8c6d679cbe28645e6c5
grebano <simonemaschio01@gmail.com>
2023-06-02 18:38:59 +0200

    update code - better hints

--- Android_app/SmartHome/app/src/main/java/com/maiot/smarthome/Activities/SettingsActivity.java
+++ Android_app/SmartHome/app/src/main/java/com/maiot/smarthome/Activities/SettingsActivity.java
@@ -58,6 +58,7 @@ public class SettingsActivity extends AppCompatActivity {
 
         // inizializzazione delle views
         initViews();
+        setHintEditText();
 
         // inizializzazione dei filtri per gli input
         ipEditTextFilter();
@@ -75,22 +76,83 @@ public class SettingsActivity extends AppCompatActivity {
      */
     private void initViews() {
         opening_time = findViewById(R.id.opening_time);
+
         closing_time = findViewById(R.id.closing_time);
+
         high_Level_threshold = findViewById(R.id.high_Level_threshold);
+
         low_Level_threshold = findViewById(R.id.low_Level_threshold);
+
         ip_shutter_1 = findViewById(R.id.ip_shutter_1);
+
         ip_shutter_2 = findViewById(R.id.ip_shutter_2);
+
         ip_shutter_3 = findViewById(R.id.ip_shutter_3);
+
         mac_shutter_1 = findViewById(R.id.mac_shutter_1);
+
         mac_shutter_2 = findViewById(R.id.mac_shutter_2);
+
         mac_shutter_3 = findViewById(R.id.mac_shutter_3);
+
         ip_lamp_1 = findViewById(R.id.ip_lamp_1);
+
         ip_lamp_2 = findViewById(R.id.ip_lamp_2);
+
         ip_lamp_3 = findViewById(R.id.ip_lamp_3);
+
         mac_lamp_1 = findViewById(R.id.mac_lamp_1);
+
         mac_lamp_2 = findViewById(R.id.mac_lamp_2);
+
         mac_lamp_3 = findViewById(R.id.mac_lamp_3);
     }
+
+    /**
+     * funzione che setta gli hint degli edit text controllando che i valori iniziali non siano nulli
+     */
+    private void setHintEditText() {
+        // TODO: 10/05/2021 settare anche gli hint per tapparelle e wifi (magari usare dei loop)
+
+        if (IpAddr_MacAddr.SHUTTER1_IP != null && !IpAddr_MacAddr.SHUTTER1_IP.equals("")) {
+            ip_shutter_1.setHint(IpAddr_MacAddr.SHUTTER1_IP);
+        }
+        if (IpAddr_MacAddr.SHUTTER2_IP != null && !IpAddr_MacAddr.SHUTTER2_IP.equals("")) {
+            ip_shutter_2.setHint(IpAddr_MacAddr.SHUTTER2_IP);
+        }
+        if (IpAddr_MacAddr.SHUTTER3_IP != null && !IpAddr_MacAddr.SHUTTER3_IP.equals("")) {
+            ip_shutter_3.setHint(IpAddr_MacAddr.SHUTTER3_IP);
+        }
+        if (IpAddr_MacAddr.SHUTTER1_MAC != null && !IpAddr_MacAddr.SHUTTER1_MAC.equals("")) {
+            mac_shutter_1.setHint(IpAddr_MacAddr.SHUTTER1_MAC);
+        }
+        if (IpAddr_MacAddr.SHUTTER2_MAC != null && !IpAddr_MacAddr.SHUTTER2_MAC.equals("")) {
+            mac_shutter_2.setHint(IpAddr_MacAddr.SHUTTER2_MAC);
+        }
+        if (IpAddr_MacAddr.SHUTTER3_MAC != null && !IpAddr_MacAddr.SHUTTER3_MAC.equals("")) {
+            mac_shutter_3.setHint(IpAddr_MacAddr.SHUTTER3_MAC);
+        }
+        if (IpAddr_MacAddr.LAMP1_IP != null && !IpAddr_MacAddr.LAMP1_IP.equals("")) {
+            ip_lamp_1.setHint(IpAddr_MacAddr.LAMP1_IP);
+        }
+        if (IpAddr_MacAddr.LAMP2_IP != null && !IpAddr_MacAddr.LAMP2_IP.equals("")) {
+            ip_lamp_2.setHint(IpAddr_MacAddr.LAMP2_IP);
+        }
+        if (IpAddr_MacAddr.LAMP3_IP != null && !IpAddr_MacAddr.LAMP3_IP.equals("")) {
+            ip_lamp_3.setHint(IpAddr_MacAddr.LAMP3_IP);
+        }
+        if (IpAddr_MacAddr.LAMP1_MAC != null && !IpAddr_MacAddr.LAMP1_MAC.equals("")) {
+            mac_lamp_1.setHint(IpAddr_MacAddr.LAMP1_MAC);
+        }
+        if (IpAddr_MacAddr.LAMP2_MAC != null && !IpAddr_MacAddr.LAMP2_MAC.equals("")) {
+            mac_lamp_2.setHint(IpAddr_MacAddr.LAMP2_MAC);
+        }
+        if (IpAddr_MacAddr.LAMP3_MAC != null && !IpAddr_MacAddr.LAMP3_MAC.equals("")) {
+            mac_lamp_3.setHint(IpAddr_MacAddr.LAMP3_MAC);
+        }
+    }
+
+
 
     /**
      * Metodo che gestisce il click sul bottone per tornare alla main activity
